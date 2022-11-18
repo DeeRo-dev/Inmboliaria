d2d7e70843098601d4295d267dc0b3823db10450
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2022-11-17 21:37:33 -0300

    Servicio de vender casi check

--- pages/vender/index.jsx
+++ pages/vender/index.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import imag from "../../public/banner.jpg";
 import img from "../../public/venta.jpg";
 import style from "../../styles/Vender.module.css"
-
+import { HiBriefcase,HiOfficeBuilding, HiShieldCheck, HiUserGroup } from "react-icons/hi";
 import {Footer} from "../../components/Footer"
  const Vender = ()=>{ 
   return (
@@ -25,14 +25,31 @@ import {Footer} from "../../components/Footer"
         </div>
         <div className={style.contentSpam}>
           <div className={style.TitleP}>
-            <h1>Titulo</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas aliquam, ratione neque minus, maxime officia dolor laborum eaque sint provident velit et magni facere natus minima, consequuntur tempora recusandae quaerat!</p>
+            <h1>Nuestro enfoque</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum dolor voluptatum, autem veniam corporis beatae blanditiirferendis illum eaque nostrum repudiandae tempora.:</p>
           </div>
           <div className={style.contentCards}>
-            <div className={style.card}></div>
-            <div className={style.card}></div>
-            <div className={style.card}></div>
-            <div className={style.card}></div>
+            <div className={style.card}>
+              <HiBriefcase className={style.icon}/>
+              <h2>Cartera</h2>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit deleniti harum distinctio autem ea molestias corporis dolorum. Excepturi aliquid enim placeat distinctio delectus! Tenetur quisquam quia similique necessitatibus error laboriosam.</p>
+              </div>
+              <div className={style.card}>
+              <HiOfficeBuilding className={style.icon}/>
+              <h2>Hoteles</h2>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit deleniti harum distinctio autem ea molestias corporis dolorum. Excepturi aliquid enim placeat distinctio delectus! Tenetur quisquam quia similique necessitatibus error laboriosam.</p>
+              </div>
+              <div className={style.card}>
+              <HiShieldCheck className={style.icon}/>
+              <h2>Seguridad</h2>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit deleniti harum distinctio autem ea molestias corporis dolorum. Excepturi aliquid enim placeat distinctio delectus! Tenetur quisquam quia similique necessitatibus error laboriosam.</p>
+              </div>
+              <div className={style.card}>
+              <HiUserGroup className={style.icon}/>
+              <h2>Clientes</h2>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit deleniti harum distinctio autem ea molestias corporis dolorum. Excepturi aliquid enim placeat distinctio delectus! Tenetur quisquam quia similique necessitatibus error laboriosam.</p>
+              </div>
+
           </div>
         </div>
       </div>
